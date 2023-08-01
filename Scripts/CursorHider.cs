3e20a353dad7f6499560b08a290eb4294bbd4753
Daniel Walls <walls.dk@gmail.com>
2023-07-31 23:34:57 -1000

    cursor hider

---
+++ Scripts/CursorHider.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace pt_player_3d.Scripts
+{
+    public class CursorHider : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private void OnDisable()
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
