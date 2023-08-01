85a2cc8e0fba7736b7034da885f8f1a089eea74f
Daniel Walls <walls.dk@gmail.com>
2023-07-31 22:14:09 -1000

    ground checking

---
+++ Scripts/GroundCheck3d.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace pt_player_3d.Scripts
+{
+    public abstract class GroundCheck3d : MonoBehaviour
+    {
+        public abstract bool TryGetGround(out GroundData3d groundData3d);
+    }
+
+    public struct GroundData3d
+    {
+        public Vector3 Normal;
+        public Vector3 Point;
+        public Collider Surface;
+    }
+}
---
+++ Scripts/GroundCheck3dDebugView.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace pt_player_3d.Scripts
+{
+    [RequireComponent(typeof(GroundCheck3d))]
+    public class GroundCheck3dDebugView : MonoBehaviour
+    {
+
+        private GroundCheck3d _groundCheck3d;
+
+        private void Start()
+        {
+            _groundCheck3d = GetComponent<GroundCheck3d>();
+        }
+
+        private void OnGUI()
+        {
+            if (_groundCheck3d.TryGetGround(out GroundData3d groundData))
+            {
+                GUILayout.Label($"{name} is grounded.");
+                GUILayout.Label($"Normal: {groundData.Normal}");
+                GUILayout.Label($"Point: {groundData.Point}");
+                GUILayout.Label($"Surface: {groundData.Surface.name}");
+            }
+            else
+            {
+                GUILayout.Label($"{name} is not grounded.");
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (Application.isPlaying && _groundCheck3d.TryGetGround(out GroundData3d groundData))
+            {
+                Gizmos.DrawSphere(groundData.Point, 0.1f);
+                Gizmos.DrawRay(groundData.Point, groundData.Normal * 2);
+            }
+        }
+    }
+}
